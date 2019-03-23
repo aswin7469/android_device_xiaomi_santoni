dfdcf73cc849f661472e8f6c2f58f5f0986b9cb4
HarukeyUA <inazarrusnak@gmail.com>
2019-03-23 11:21:26 +0200

    Lineage bringup

--- lineage_santoni.mk
+++ lineage_santoni.mk
@@ -14,15 +14,18 @@
 # limitations under the License.
 #
 
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common Syberia stuff.
-$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := syberia_santoni
+PRODUCT_NAME := lineage_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
@@ -30,9 +33,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 TARGET_VENDOR_PRODUCT_NAME := santoni
 
-TARGET_BOOT_ANIMATION_RES := 720
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys"
+    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.10.0.NAMMIFD release-keys" \
 
 BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.10.0.NAMMIFD:user/release-keys"
